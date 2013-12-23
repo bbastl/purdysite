46eec99016a38b52e4a0cf1112a9e93330ab510e
valladium <sdefazio@uplift.com>
2013-12-23 12:58:27 -0800

    leds

--- app/models/product_leds_segment.rb
+++ app/models/product_leds_segment.rb
@@ -2,6 +2,6 @@ class ProductLedsSegment < ActiveRecord::Base
 	self.table_name = "product_leds_green_segment"
 
 	def self.to_display
-		self.all.order("sort").order(:led).order(:item_type)
+		self.all.order(:item_type, :led, :sort)
 	end
 end
