c1e3a4a8bbdf5758de36a681e4af65626574d7fe
shayne <shayne@shayne-Dell-System-XPS-15Z.(none)>
2014-01-29 19:51:43 -0800

    added pdfs to git

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -23,6 +23,20 @@ class AdminController < ApplicationController
 
  	def product_update
  		Rails.logger.debug params
+
+ 		product_type = params[:product_type]
+ 		class_to_load = product_type.camelize.constantize
+ 		product = class_to_load.where(product_id: params[:product_id]).first
+
+
+ 		filtered_params = params.require(product_type.to_sym).permit(product.attribute_names)
+
+ 		begin
+ 			product.update(filtered_params)
+ 		rescue StandardError => error
+ 			Rails.logger.error error.backtrace
+ 		end
+
  		flash[:alert] = "UPDATED"
  		redirect_to "/admin/product_edit_item?product_type=#{params[:product_type]}&product_id=#{params[:product_id]}"
  	end
@@ -64,8 +78,35 @@ class AdminController < ApplicationController
  			Rails.logger.debug order.inspect
  			order.status = params[:hStatus] unless params[:hStatus].empty?
  			order.project = params[:hProject] unless params["hProject"].empty?
+ 			order.decision_maker = params[:mmaker] unless params[:mmaker].empty?
+ 			order.decision_title = params[:ttitle] unless params[:ttitle].empty?
+ 			order.target_price = params[:Target_Price] unless params[:Target_Price].empty?
+ 			order.buying_criteria = params[:Buying_Criteria] unless params[:Buying_Criteria].empty?
+ 			order.how_did = params[:How_Did] unless params[:How_Did].empty?
+ 			order.competition1 = params[:Competition1] unless params[:Competition1].empty?
+ 			order.competition2 = params[:Competition2] unless params[:Competition2].empty?
+ 			order.competition3 = params[:Competition3] unless params[:Competition3].empty?
+ 			order.competition4 = params[:Competition4] unless params[:Competition4].empty?
+ 			#order.prototype_date = params[:Pr_Date] unless params[:Pr_Date].empty?
+ 			#order.production_start = params[:P_Date] unless params[:P_Date].empty?
  			order.eau = params[:tEau] unless params["tEau"].empty?
+
+ 			# SHIPING INFO
+ 			order.auth_status = params[:hAuthStatus] unless params[:hAuthStatus].empty?
+ 			order.auth_by = params[:fauthby] unless params[:fauthby].empty?
+ 			order.shippingpriority = params[:sPriority] unless params[:sPriority].empty?
+ 			order.shippingnumber = params[:hShipping] unless params[:hShipping].empty?
+ 			order.shippingmethod = params[:sMethod] unless params[:sMethod].empty?
+ 			order.shippingcarrier = params[:sCarrier] unless params[:sCarrier].empty?
  			order.save
+
+ 			#Handle create new note
+ 			note = params[:hNote] unless params[:hNote].empty?
+ 			note = params[:hNote2] unless params[:hNote2].empty?
+
+ 			if note
+ 				OrderNote.create(order_id: order.id, note_date: Time.now, details: note, role: "admin")
+ 			end
  		end
 
  		redirect_to "/admin/order_edit?orderid=#{params[:orderid]}" 
